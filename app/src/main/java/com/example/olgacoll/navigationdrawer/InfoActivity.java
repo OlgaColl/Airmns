bbdc106c1d17d09183b5345a13132d28c4d5fe82
OlgaColl <ocoll94@gmail.com>
2017-03-28 10:42:33 +0200

    Add navigation menu

---
+++ app/src/main/java/com/example/olgacoll/navigationdrawer/InfoActivity.java
@@ -0,0 +1,21 @@
+package com.example.olgacoll.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by olgacoll on 28/3/17.
+ */
+
+public class InfoActivity extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_app, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/olgacoll/navigationdrawer/IntroduceAvaibilityActivity.java
@@ -0,0 +1,16 @@
+package com.example.olgacoll.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IntroduceAvaibilityActivity extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduce_avaibility, container, false);
+    }
+}
